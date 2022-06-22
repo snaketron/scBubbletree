67144c32098af7814433b9b97bdcf7e0bb4a14fa
sktron <simo.kitanovski@uni-due.de>
2022-06-22 12:43:28 +0200

    test gap confirmation

--- test_gap.R
+++ test_gap.R
@@ -1,19 +1,4 @@
-source(file = "~/Rutil/Init_Rpack.R")
-source(file = "../scBubbletree/R/util.R")
-source(file = "../scBubbletree/R/main.R")
-source(file = "../scBubbletree/R/annotation.R")
-library(cluster, lib.loc = lib.loc)
-library(Seurat, lib.loc = lib.loc)
-library(ggplot2, lib.loc = lib.loc)
-library(reshape2, lib.loc = lib.loc)
-library(parallel, lib.loc = lib.loc)
-library(ape, lib.loc = lib.loc)
-library(ggtree, lib.loc = lib.loc)
-library(org.Hs.eg.db, lib.loc = lib.loc)
-library(bluster, lib.loc = lib.loc)
-library(SummarizedExperiment, lib.loc = lib.loc)
-
-
+require(scBubbletree)
 
 a <- matrix(data = rnorm(n = 500*8, mean = 0, sd = 1), ncol = 8)
 b <- rbind(matrix(data = rnorm(n = 250*1, mean = 0, sd = 1), ncol = 1),
@@ -24,50 +9,60 @@ a <- cbind(b, c, a)
 rm(b,c)
 
 
-b_test_0 <- get_k(B = 20,
-                  cv_prop = 1,
-                  ks = 1:20,
-                  x = a,
-                  n_start = 10,
-                  iter_max = 50,
-                  cores = 5,
-                  mini_output = T,
-                  approx_silhouette = F)
-
-ggplot(data = b_test_0$sil_stats_summary)+
-  geom_point(aes(x = k, y = sil_mean))+
-  geom_errorbar(aes(x = k, y = sil_mean, ymin = L95, ymax = H95), width = 0.1)+
-  ggtitle(label = "Silhouette")+
-  theme_bw()+
-  ylab(label = "Average Silhouette")|
-  ggplot(data = b_test_0$gap_stats_summary)+
-  geom_point(aes(x = k, y = gap_mean))+
-  geom_errorbar(aes(x = k, y = gap_mean, ymin = L95, ymax = H95), width = 0.1)+
-  ggtitle(label = "Gap")+
-  theme_bw()+
-  ylab(label = "Average Gap statistic")|
-  ggplot(data = b_test_0$wss_stats_summary)+
-  geom_point(aes(x = k, y = wss_mean))+
-  geom_errorbar(aes(x = k, y = wss_mean, ymin = L95, ymax = H95), width = 0.1)+
-  ggtitle(label = "WSS")+
-  theme_bw()+
-  ylab(label = "Average WSS")
-
-
 
 
 require(cluster)
-gaps <- cluster::clusGap(x = a, FUNcluster = kmeans, K.max = 20,
-                         B = 100, d.power = 1,
+gaps <- cluster::clusGap(x = a,
+                         FUNcluster = kmeans,
+                         K.max = 10,
+                         iter.max = 100,
+                         B = 50,
+                         d.power = 1,
                          spaceH0 = "original")
 plot(gaps)
+gap_cluster <- data.frame(gaps$Tab)
+gap_cluster$k <- as.numeric(rownames(gap_cluster))
+
+
+ggplot()+
+  geom_point(data = gap_cluster,
+             aes(x = k, y = gap))+
+  geom_errorbar(data = gap_cluster,
+                aes(x = k, y = gap, ymin = gap-SE.sim,
+                    ymax = gap+SE.sim), width = 0.3)
+
+
+b_test_0 <- get_k(B = 5,
+                  cv_prop = 1,
+                  ks = 1:10,
+                  x = a,
+                  cores = 4,
+                  mini_output = F,
+                  B_gap = 100)
 
 
 
-sils <- c()
-for(k in 2:20) {
-  sils <- c(sils, mean(cluster::silhouette(x = kmeans(x = a, centers = k)$cluster,
-                           dist = dist(a, method = "euclidean"))[, 3]))
-}
-plot(sils)
+ggplot()+
+  geom_point(data = b_test_0$gap_stats_summary,
+             aes(x = k, y = gap_mean))+
+  geom_errorbar(data = b_test_0$gap_stats_summary,
+                aes(x = k, y = gap_mean, ymin = L95, ymax = H95), width = 0.3)+
+  geom_point(data = b_test_0$gap_stats,
+             aes(x = k, y = gap), size = 0.25, col = "red",
+             position = position_jitter(width = 0.25, height = 0))+
+  theme_bw()+
+  ylab(label = "Average Gap")|
+ggplot(data = b_test_0$wcss_stats_summary)+
+  geom_point(aes(x = k, y = wcss_mean))+
+  geom_errorbar(aes(x = k, y = wcss_mean, ymin = L95, ymax = H95), width = 0.1)+
+  theme_bw()+
+  scale_y_log10()+
+  ylab(label = "Average WCSS")
+
+
+
+
+plot(b_test_0$gap_stats_summary$gap_mean,
+     gap_cluster$gap);abline(0,1)
+
 
