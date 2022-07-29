096d192b4e228ddcecac19ab9d7fa0eea5033030
sktron <simo.kitanovski@uni-due.de>
2022-07-29 15:57:42 +0200

    more tests on cat_tiles

--- tests/testthat/test-input-get-cat-tiles.R
+++ tests/testthat/test-input-get-cat-tiles.R
@@ -744,3 +744,656 @@ test_that("btd argument", {
 })
 
 
+
+
+
+
+test_that("f argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  f <- sample(x = LETTERS[1:5], size = 99, replace = T)
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "length of f is not equal to number of cells in btd")
+
+
+
+  f <- NA
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "f must be a character vector")
+
+
+
+  f <- NULL
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "f must be a character vector")
+
+
+
+  f <- 1:100
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "f must be a character vector")
+
+
+
+  f <- as.factor(1:100)
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "f must be a character vector")
+
+
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+  f[1] <- NA
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "f must be a character vector")
+
+
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+  f[1] <- Inf
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  f <- sample(x = LETTERS[1:5], size = 110, replace = T)
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "length of f is not equal to number of cells in btd")
+
+})
+
+
+
+
+
+
+test_that("integrate_vertical argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = T,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = 1,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "integrate_vertical is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = "1",
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "integrate_vertical is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = Inf,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "integrate_vertical is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = c(T, F),
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "integrate_vertical is a logical parameter \\(TRUE or FALSE\\)")
+
+
+})
+
+
+
+
+
+
+
+test_that("show_hclust argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = T,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = 1,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "show_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = "1",
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "show_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = Inf,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "show_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = c(T, F),
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "show_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+})
+
+
+
+
+
+test_that("disable_hclust argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = T,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = 1,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "disable_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = "1",
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "disable_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = Inf,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "disable_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = c(T, F),
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "disable_hclust is a logical parameter \\(TRUE or FALSE\\)")
+
+
+})
+
+
+
+
+
+test_that("tile_bw argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = 1,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "tile_bw is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = "1",
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "tile_bw is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = Inf,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "tile_bw is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = c(T, F),
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    "tile_bw is a logical parameter \\(TRUE or FALSE\\)")
+
+
+})
+
+
+
+
+
+test_that("rotate_x_axis_labels argument", {
+
+  # build bubbletree
+  btd <- get_bubbletree(x = matrix(data = rnorm(n = 100*10), ncol = 10),
+                        k = 3,
+                        B = 20,
+                        N_eff = 50,
+                        n_start = 100,
+                        iter_max = 100,
+                        kmeans_algorithm = "MacQueen",
+                        cores = 1,
+                        seed = NULL,
+                        round_digits = 2,
+                        show_simple_count = F)
+
+  f <- sample(x = LETTERS[1:5], size = 100, replace = T)
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = T),
+    NA)
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = 1),
+    "rotate_x_axis_labels is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = "1"),
+    "rotate_x_axis_labels is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = Inf),
+    "rotate_x_axis_labels is a logical parameter \\(TRUE or FALSE\\)")
+
+
+
+  expect_error(get_cat_tiles(
+    btd = btd,
+    f = f,
+    integrate_vertical = F,
+    round_digits = 2,
+    show_hclust = F,
+    disable_hclust = F,
+    tile_text_size = 3,
+    tile_bw = F,
+    x_axis_name = "Feature",
+    rotate_x_axis_labels = c(T, F)),
+    "rotate_x_axis_labels is a logical parameter \\(TRUE or FALSE\\)")
+
+
+})
