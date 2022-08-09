5a680c26751d9b93e11c7fff847d29e2578faf38
Simo Kitanovski <simo.kitanovski@uni-due.de>
2022-08-09 14:50:36 +0200

    last unit tests get_gini

--- tests/testthat/test-input-get-gini.R
+++ tests/testthat/test-input-get-gini.R
@@ -125,22 +125,22 @@ test_that("labels argument", {
   # labels as factors
   clusters <- base::sample(x = base::LETTERS[1:3],
                            size = 500, replace = T)
-  labels <- as.factors(base::sample(x = base::LETTERS[1:3],
-                                    size = 500, replace = T))
+  labels <- as.factor(base::sample(x = base::LETTERS[1:3],
+                                   size = 500, replace = T))
   expect_error(get_gini(labels = labels,
                         clusters = clusters),
-               NA)
+               "labels can only contain characters or numbers")
 
 
   # labels as logicals
   clusters <- base::sample(x = base::LETTERS[1:3],
                            size = 500, replace = T)
   labels <- base::sample(x = c(T, F), size = 500, replace = T)
-  expect_error(get_gini(labels = labels, clusters = clusters),
-               "labels can only contain characters, factors or numbers")
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters),
+               "labels can only contain characters or numbers")
 
 })
-
 
 
 
@@ -153,64 +153,82 @@ test_that("clusters argument", {
                          size = 500, replace = T)
 
   # NA element
-  labels_test <- labels
-  labels_test[1] <- NA
-  expect_error(get_gini(labels = labels_test,
-                        clusters = clusters),
-               "labels cannot have INF\\/NA\\/NULL values")
+  clusters_test <- clusters
+  clusters_test[1] <- NA
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters_test),
+               "clusters cannot have INF\\/NA\\/NULL values")
+
 
   # INF element but as character
-  labels_test <- labels
-  labels_test[1] <- Inf
-  expect_error(get_gini(labels = labels_test,
-                        clusters = clusters),
+  clusters_test <- clusters
+  clusters_test[1] <- Inf
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters_test),
                NA)
 
 
-  labels <- base::sample(x = 1:3,
-                         size = 500, replace = T)
+  clusters <- base::sample(x = 1:3, size = 500, replace = T)
   # NA element
-  labels_test <- labels
-  labels_test[1] <- NA
-  expect_error(get_gini(labels = labels_test,
-                        clusters = clusters),
-               "labels cannot have INF\\/NA\\/NULL values")
+  clusters_test <- clusters
+  clusters_test[1] <- NA
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters_test),
+               "clusters cannot have INF\\/NA\\/NULL values")
+
 
   # INF element
-  labels_test <- labels
-  labels_test[1] <- Inf
-  expect_error(get_gini(labels = labels_test,
-                        clusters = clusters),
-               "labels cannot have INF\\/NA\\/NULL values")
+  clusters_test <- clusters
+  clusters_test[1] <- Inf
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters_test),
+               "clusters cannot have INF\\/NA\\/NULL values")
 
   # negative element
-  labels_test <- labels
-  labels_test[1] <- -5
-  expect_error(get_gini(labels = labels_test,
-                        clusters = clusters),
+  clusters_test <- clusters
+  clusters_test[1] <- -5
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters_test),
                NA)
 
 
+  expect_error(get_gini(labels = labels,
+                        clusters = 1),
+               "clusters must be a vector with more than one element")
 
-
-  expect_error(get_gini(labels = 1,
-                        clusters = clusters),
-               "labels must be a vector with more than one element")
-
-  expect_error(get_gini(labels = c(1, 1),
-                        clusters = clusters),
+  expect_error(get_gini(labels = labels,
+                        clusters = c(1, 1)),
                "labels and clusters must be equal-length vectors")
 
-  expect_error(get_gini(labels = rep(1, times=length(clusters)),
+  expect_error(get_gini(labels = labels,
+                        clusters = rep(1, times=length(clusters))),
+               NA)
+
+  expect_error(get_gini(labels=labels,
+                        clusters=rep(1, times=length(clusters)))$wgi==0,
+               NA)
+
+  expect_error(all(get_gini(labels = labels,
+                            clusters = rep(1, times=length(clusters)))$gi==0),
+               NA)
+
+
+  # clusters as factors
+  labels <- base::sample(x = base::LETTERS[1:3],
+                           size = 500, replace = T)
+  clusters <- as.factor(base::sample(x = base::LETTERS[1:3],
+                                   size = 500, replace = T))
+  expect_error(get_gini(labels = labels,
                         clusters = clusters),
-               NA)
+               "clusters can only contain characters or numbers")
 
-  expect_error(get_gini(labels = rep(1, times=length(clusters)),
-                        clusters = clusters)$wgi==0,
-               NA)
 
-  expect_error(all(get_gini(labels = rep(1, times=length(clusters)),
-                            clusters = clusters)$gi==0),
-               NA)
+  # labels as logicals
+  labels <- base::sample(x = base::LETTERS[1:3],
+                           size = 500, replace = T)
+  clusters <- base::sample(x = c(T, F), size = 500, replace = T)
+  expect_error(get_gini(labels = labels,
+                        clusters = clusters),
+               "clusters can only contain characters or numbers")
 
 })
